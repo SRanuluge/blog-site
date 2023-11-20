d4304dcda5905e2ec3106119672a1d328bb26b7c
SRanuluge <sranuluge.dev@gmail.com>
2023-11-20 19:19:34 +1300

    added constants

--- app/core/config.php
+++ app/core/config.php
@@ -1,8 +1,20 @@
 <?php
 
-// function show($stuff)
-// {
-//     echo '<pre>';
-//     print_r($stuff);
-//     echo '</pre>';
-// }
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // Database config
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+    define('ROOT', 'http://localhost/php-aws-ec2/public');
+} else {
+    /** database config */
+    define('DBNAME', 'my_db');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+
+    define('ROOT', 'https://www.yourwebsite.com');
+}
